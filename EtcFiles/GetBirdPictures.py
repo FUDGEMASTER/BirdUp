9343bb1391a480b7710f9e10dae7138255cfa866
Kirby <kirby.forsberg@hotmail.com>
2020-03-08 18:29:40 -0400

    fixed GetBirdPics

--- EtcFiles/GetBirdPictures.py
+++ EtcFiles/GetBirdPictures.py
@@ -5,15 +5,13 @@ This file just calls BBID to download 50 images of each kind of bird
 from subprocess import run
 from shutil import move
 from os import mkdir, listdir, path
+from pathlib import Path
 
 num_of_images_per_search = 50
 
-source = r'C:\Users\kirby\Desktop\BirdUp\bing'
-parent_dir = r'C:\Users\kirby\Desktop\BirdUp\Bird Pictures'
-try:
-    mkdir(parent_dir)
-except FileExistsError:
-    pass
+this_file = path.abspath(path.dirname(__file__))
+source = this_file / Path(r'bing')
+parent_dir = this_file / Path(r'Completed Queries')
 
 species = ["Black Capped Chickadee", "Dark Eyed Junco", "Downy Woodpecker", "Male Northern Cardinal", "Mourning Dove", "Blue Jay", "White Breasted Nuthatch", "American Robin", "Song Sparrow", "Common Grackle",
            "American Goldfinch", "Red Bellied Woodpecker", "House Finch", "Tufted Titmouse", "Hairy Woodpecker", "European Starling", "House Sparrow", "Red Winged Blackbird", "American Crow", "Brown Headed Cowbird", "Purple Finch"]
@@ -21,13 +19,16 @@ species = ["Black Capped Chickadee", "Dark Eyed Junco", "Downy Woodpecker", "Mal
 # for testing
 # species = ["Northern Cardinal", "American Goldfinch"]
 
-for i in range(len(species)):
-    new_bird_folder = path.join(parent_dir, species[i])
+def search_and_dump(query, quantity):
+    new_search_folder = parent_dir / Path('{}'.format(query))
     try:
-        mkdir(new_bird_folder)
+        mkdir(new_search_folder)
     except FileExistsError:
         pass
-    run(['python', r'C:\Users\kirby\Desktop\BirdUp\EtcFiles\BBID.py', '-s', '\"{}\"'.format(species[i]), '--limit', '{}'.format(num_of_images_per_search)]) # downloads 50(ish) images and puts them in 'bing'
-    files = listdir(source)# lists all files in bing
-    move(source, new_bird_folder)
+    run(['python', this_file / Path(r''), '-s',
+         '\"{}\"'.format(query), '-o', '{}'.format(new_search_folder), '--limit', '{}'.format(quantity)])
+
+for i in range(len(species)):
+    search_and_dump(species[i], num_of_images_per_search)
+
   

1d8db2c814aee47fe936b3739c17b64834f3e4e4
elgarcia27 <45183156+elgarcia27@users.noreply.github.com>
2020-03-11 15:28:42 -0400

    Update DDL.sql

--- Database/DDL.sql
+++ Database/DDL.sql
@@ -4,138 +4,208 @@ DROP TABLE IF EXISTS Bird;
 
 DROP TABLE IF EXISTS Feed_Mix;
 
-DROP TABLE IF EXISTS Location;
-
-CREATE TABLE Entry (
-    entry_Id            INTEGER       REFERENCES Bird (bird_Id),
-    common_Name         VARCHAR (50),
-    wiki_link           VARCHAR (255),
-    conservation_Status VARCHAR (50),
-    eats                INT           REFERENCES Feed_Mix (feed_Id),
-    place_Seen          INT           REFERENCES Location (location_Id),
-    seen_Date           DATETIME      DEFAULT CURRENT_DATE,
-    seen_time           DATETIME      DEFAULT CURRENT_TIME
+CREATE TABLE Entry(
+entry_Id INTEGER REFERENCES Bird(bird_Id),
+common_Name varchar (50),
+wiki_link varchar(255),
+conservation_Status varchar (50) ,
+eats int REFERENCES Feed_Mix(feed_Id),
+place_Seen VARCHAR(255),
+seen_Date DATETIME DEFAULT CURRENT_DATE ,
+seen_time DATETIME DEFAULT CURRENT_TIME
 );
 
 INSERT INTO Entry (
-                      entry_Id,
-                      common_Name,
-                      wiki_link,
-                      conservation_Status,
-                      eats,
-                      place_Seen,
-                      seen_Date,
-                      seen_Time
-                  )
-                  VALUES (
-                      1,
-                      'Blue Jay',
-                      'https://avibase.bsc-eoc.org/species.jsp?avibaseid=1DA430B8663CD4BE',
-                      NULL,
-                      2,
-                      1,
-                      CURRENT_DATE,
-                      CURRENT_TIME
-                  ),
-                  (
-                      2,
-                      'HOUSE SPARROW',
-                      'https://avibase.bsc-eoc.org/species.jsp?avibaseid=240E33900CE34D44',
-                      'INTRODUCED',
-                      3,
-                      2,
-                      CURRENT_DATE,
-                      CURRENT_TIME
-                  ),
-                  (
-                      3,
-                      'northern-cardinal',
-                      'https://avibase.bsc-eoc.org/species.jsp?avibaseid=4E74AE2276258520',
-                      NULL,
-                      1,
-                      3,
-                      CURRENT_DATE,
-                      CURRENT_TIME
-                  );
+entry_Id , 
+common_Name , 
+wiki_link , 
+conservation_Status, 
+eats , 
+place_Seen , 
+seen_Date , 
+seen_Time)
+VALUES
+(
+1 , 
+'Blue Jay' , 
+'https://avibase.bsc-eoc.org/species.jsp?avibaseid=1DA430B8663CD4BE' , 
+null , 
+2 , 
+'North Providence', 
+CURRENT_DATE , 
+CURRENT_TIME
+) ,
+(2 , 
+'HOUSE SPARROW' ,
+'https://avibase.bsc-eoc.org/species.jsp?avibaseid=240E33900CE34D44' , 
+'INTRODUCED', 
+3 , 
+'Providence' , 
+CURRENT_DATE , 
+CURRENT_TIME ) , 
+(3 , 
+'northern-cardinal' , 
+'https://avibase.bsc-eoc.org/species.jsp?avibaseid=4E74AE2276258520' , 
+null , 
+1 , 
+'Warwick' , 
+CURRENT_DATE , 
+CURRENT_TIME )
+;
 
-CREATE TABLE Bird (
-    bird_Id             INTEGER      PRIMARY KEY,
-    common_Name         VARCHAR (50),
-    scientific_Name     VARCHAR (50),
-    conservation_Status VARCHAR (50),
-    eats                INT          REFERENCES Feed_Mix (feed_Id) 
+CREATE TABLE Bird(
+bird_Id INTEGER PRIMARY KEY ,
+common_Name varchar (50) ,
+scientific_Name varchar (50) ,
+conservation_Status varchar (50) ,
+eats INTEGER REFERENCES Feed_Mix(feed_Id)
 );
 
-INSERT INTO Bird (
-                     common_Name,
-                     scientific_Name,
-                     conservation_Status,
-                     eats
-                 )
-                 VALUES (
-                     'Blue Jay',
-                     'cyannociatta cristata',
-                     NULL,
-                     2
-                 ),
-                 (
-                     'house sparrow',
-                     'something-sciency',
-                     'introduced',
-                     3
-                 ),
-                 (
-                     'northern-cardinal',
-                     'somewhat sciency',
-                     NULL,
-                     1
-                 );
+INSERT INTO Bird( 
+common_Name , 
+scientific_Name ,  conservation_Status , 
+eats
+)
+VALUES
+(
+'Blue Jay', 
+'Cyanociatta Cristata', 
+'Least Concern', 
+6),
+(
+'House Sparrow', 
+'Passer Domesticus', 
+'Least Concern', 
+3),
+(
+'Northern Cardinal', 
+'Cardinalis Cardinalis', 
+'Least Concern', 
+3),
+(
+'Black-Capped Chickadee', 
+'Poecile Atricapillus',
+'Least Concern', 
+3),
+(
+'Dark-Eyed Junco', 
+'Junco Hyemalis', 
+'Least Concern', 
+5),
+(
+'Downy Woodpecker', 
+'Dryobates Pubescens', 
+'Least Concern', 
+6),
+(
+'Mourning Dove', 
+'Zenaida Macroura', 
+'Least Concern',
+3),
+(
+'White-Breasted Nuthatch',
+'Sitta Carolinensis', 
+'Least Concern', 
+3),
+(
+'American Goldfinch', 
+'Spinus Tristis', 
+'Least Concern', 
+5),
+(
+'Red-Bellied Woodpecker', 
+'Melanerpes Carolinus', 
+'Least Concern', 
+3),
+(
+'House Finch', 
+'Haemorhous Mexicanus', 
+'Least Concern', 
+8),
+(
+'Tufted Titmouse', 
+'Baeolophus Bicolor', 
+'Least Concern', 
+8),
+(
+'Hairy Woodpecker', 
+'Dryobates Villosus', 
+'Least Concern', 
+6),
+(
+'European Starling', 
+'Sturnus Vulgaris', 
+'Least Concern',
+2),
+(
+'American Robin', 
+'Turdus Migratorius', 
+'Least Concern',
+5),
+(
+'Song Sparrow', 
+'Melospiza Melodia', 
+'Least Concern', 
+8),
+(
+'Common Grackle', 
+'Quiscalus Quiscula', 
+'Near Threatened', 
+6),
+(
+'Red-Winged Blackbird', 
+'Hgelaius Phoniceus',
+'Least Concern', 
+1),
+(
+'American Crow', 
+'Corvus Brachyrhynclos', 
+'Least Concern', 
+5),
+(
+'Brown-Headed Cowbird', 
+'Molothrus Ater',
+'Least Concern', 
+3),
+(
+'Purple Finch', 
+'Haemorhous Purpureus', 
+'Least Concern', 
+5)
+;
 
-CREATE TABLE Feed_Mix (
-    feed_Id   INTEGER      PRIMARY KEY,
-    feed_Type VARCHAR (50) 
+CREATE TABLE Feed_Mix(
+feed_Id INTEGER PRIMARY KEY ,
+feed_Type varchar (50)
 );
 
-INSERT INTO Feed_Mix (
-                         feed_Type
-                     )
-                     VALUES (
-                         'millet'
-                     ),
-                     (
-                         'suet'
-                     ),
-                     (
-                         'black oil sunflower seed'
-                     ),
-                     (
-                         'corn'
-                     );
+INSERT INTO Feed_Mix( 
+feed_Type
+) 
+VALUES(
+'Millet'
+) ,
+(
+'Suet'
+) ,
+(
+'Black Oil Sunflower Seed'
+) ,
+(
+'Corn'
+),
+(
+'Hulled Sunflower Seed'
+),
+(
+'Safflower'
+),
+( 
+'Milo'
+),
+( 
+'Nyjer'
+)
+;
 
-CREATE TABLE Location (
-    location_Id INTEGER      PRIMARY KEY,
-    zip         VARCHAR (10),
-    city        VARCHAR (30),
-    state       VARCHAR (20) 
-);
 
-INSERT INTO Location (
-                         zip,
-                         city,
-                         state
-                     )
-                     VALUES (
-                         '02893',
-                         'warwick',
-                         'RI'
-                     ),
-                     (
-                         '02903',
-                         'cranston',
-                         'RI'
-                     ),
-                     (
-                         '02908',
-                         'providence',
-                         'RI'
-                     );

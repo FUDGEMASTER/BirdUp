3ee1b66f39701d6b203a0fbebdac544ae01a3dc8
Kirby <kirby.forsberg@hotmail.com>
2020-03-31 21:48:21 -0400

    renamed all bird filenames

--- EtcFiles/Completed Queries/Rename_bird_files.py
+++ EtcFiles/Completed Queries/Rename_bird_files.py
@@ -1,6 +1,7 @@
 import os 
 from pathlib import Path
 import subprocess
+
 bird_species = [
     'American Crow',
     'American Goldfinch',
@@ -17,7 +18,7 @@ bird_species = [
     'House Sparrow',
     'Male Northern Cardinal',
     'Mourning Dove',
-    'Purle Finch',
+    'Purple Finch',
     'Red Bellied Woodpecker',
     'Red Winged Blackbird',
     'Song Sparrow',
@@ -25,14 +26,40 @@ bird_species = [
     'White Breasted Nuthatch'
 ]
 
+
+bird_species_files = [
+    Path(r'C:\Users\kirby\Desktop\BirdUp\EtcFiles\Completed Queries\American Crow'),
+    Path(r'C:\Users\kirby\Desktop\BirdUp\EtcFiles\Completed Queries\American Goldfinch'),
+    Path(r'C:\Users\kirby\Desktop\BirdUp\EtcFiles\Completed Queries\American Robin'),
+    Path(r'C:\Users\kirby\Desktop\BirdUp\EtcFiles\Completed Queries\Black Capped Chickadee'),
+    Path(r'C:\Users\kirby\Desktop\BirdUp\EtcFiles\Completed Queries\Blue Jay'),
+    Path(r'C:\Users\kirby\Desktop\BirdUp\EtcFiles\Completed Queries\Brown Headed Cowbird'),
+    Path(r'C:\Users\kirby\Desktop\BirdUp\EtcFiles\Completed Queries\Common Grackle'),
+    Path(r'C:\Users\kirby\Desktop\BirdUp\EtcFiles\Completed Queries\Dark Eyed Junco'),
+    Path(r'C:\Users\kirby\Desktop\BirdUp\EtcFiles\Completed Queries\Downy Woodpecker'),
+    Path(r'C:\Users\kirby\Desktop\BirdUp\EtcFiles\Completed Queries\European Starling'),
+    Path(r'C:\Users\kirby\Desktop\BirdUp\EtcFiles\Completed Queries\Hairy Woodpecker'),
+    Path(r'C:\Users\kirby\Desktop\BirdUp\EtcFiles\Completed Queries\House Finch'),
+    Path(r'C:\Users\kirby\Desktop\BirdUp\EtcFiles\Completed Queries\House Sparrow'),
+    Path(r'C:\Users\kirby\Desktop\BirdUp\EtcFiles\Completed Queries\Male Northern Cardinal'),
+    Path(r'C:\Users\kirby\Desktop\BirdUp\EtcFiles\Completed Queries\Mourning Dove'),
+    Path(r'C:\Users\kirby\Desktop\BirdUp\EtcFiles\Completed Queries\Purple Finch'),
+    Path(r'C:\Users\kirby\Desktop\BirdUp\EtcFiles\Completed Queries\Red Bellied Woodpecker'),
+    Path(r'C:\Users\kirby\Desktop\BirdUp\EtcFiles\Completed Queries\Red Winged Blackbird'),
+    Path(r'C:\Users\kirby\Desktop\BirdUp\EtcFiles\Completed Queries\Song Sparrow'),
+    Path(r'C:\Users\kirby\Desktop\BirdUp\EtcFiles\Completed Queries\Tufted Titmouse'),
+    Path(r'C:\Users\kirby\Desktop\BirdUp\EtcFiles\Completed Queries\White Breasted Nuthatch')
+]
+
 base_path = Path(r'C:\Users\kirby\Desktop\BirdUp\EtcFiles\Completed Queries')
 
-for j in range(len(bird_species)):
-    i = 0
-    subprocess.call(['cd', '{}'.format(base_path / bird_species[j])])
-    for filename in os.listdir(base_path / bird_species[j]):
-        new_filename = bird_species[j] + "_" + str(i) + ".jpg"
-        os.rename(filename, new_filename) 
-        i -= -1
-        
+i = 0
+j = 20
+# for j in range(len(bird_species)):
+#     os.system('cd {}'.format(bird_species_files[j]))
+for filename in os.listdir(base_path / bird_species[j]):
+    new_filename = bird_species[j] + "_" + str(i) + ".jpg"
+    os.rename(filename, new_filename) 
+    i -= -1
+    
   
